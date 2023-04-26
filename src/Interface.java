cb30cd160ff9e3d66d062d6f377eae9813abc087
BT_2N8_04 <s>
2023-04-26 16:27:42 -0400

    sigh

--- src/Interface.java
+++ src/Interface.java
@@ -1,36 +1,40 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import static java.lang.System.exit;
 import static java.lang.System.in;
 
 public class Interface {
-    private String[] COMMANDS = {"install-drive", "list-drives", "pvcreate", "pvlist", "vgcreate", "vgextend", "vglist", "lvcreate", "lvlist"};
+    private final String[] COMMANDS = {"install-drive", "list-drives", "pvcreate", "pvlist", "vgcreate", "vgextend", "vglist", "lvcreate", "lvlist"};
     private ArrayList<PHD> installedPHDs;
+    private ArrayList<PHD> freePHDs;
+    private ArrayList<PV> madePVs;
+    private ArrayList<PV> freePVs;
     public void run(){
         installedPHDs = new ArrayList<>();
+        madePVs = new ArrayList<>();
+        freePVs = new ArrayList<>();
         System.out.println("Welcome to the Logical Volume Manager.");
         while(true) {
             System.out.print("Enter cmd or type help to see available commands: ");
-            String input = new Scanner(System.in).nextLine();
-            if (input.equals("exit")) exit(-1);
-            String command = "";
-            String theRest = "";
-            if(!input.contains(" ")) command = input;
-            else {
-                command = input.substring(0,input.indexOf(" "));
-                theRest = input.substring(input.indexOf(" ") + 1);
-            }
-            System.out.println("command........."+command);
-            switch (command) {
+            String[] s = new Scanner(System.in).nextLine().split(" ");
+            System.out.println(Arrays.toString(s));
+            if (s[0].equals("exit")) exit(-1);
+            switch (s[0]) {
                 case "install-drive" -> {
-                    installedPHDs.add(new PHD(theRest.substring(0, theRest.indexOf(" ")), Integer.parseInt(theRest.substring(theRest.indexOf(" ") + 1, theRest.length() - 1))));
+                    if(s.length == 3) {
+                        installedPHDs.add(new PHD(s[1],Integer.parseInt(s[2].substring(0,s[2].length()-1))));
+                        //freePHDs.add(installedPHDs.get(installedPHDs.size()-1));//to add reference to same object to another list
+                    }
                 }
                 case "list-drives" -> {
                     for(PHD p : installedPHDs) System.out.println(p.toString());
                 }
                 case "pvcreate" -> {
-
+                    if(s.length == 3){
+                        //installedPHDs.in
+                    }
                 }
                 case "pvlist" -> System.out.println("cheese");
                 case "vgcreate" -> System.out.println("cheese");
@@ -38,6 +42,7 @@ public class Interface {
                 case "vglist" -> System.out.println("cheese");
                 case "lvcreate" -> System.out.println("cheese");
                 case "lvlist" -> System.out.println("cheese");
+                default -> System.out.println("oh no");
             }
         }
     }
